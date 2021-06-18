9e987339b1805d4f9b52dba00cb1b125b42d6251
Admin <100742869@qq.com>
2021-06-18 18:07:22 +0800

    项目环境搭建

---
+++ mall-common/src/main/java/Main.java
@@ -0,0 +1,8 @@
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
