4335d4bdf7fd9b6dbc50f9467597967665adfa94
Admin <100742869@qq.com>
2021-06-19 14:43:17 +0800

    逆向生成代码

--- mall-common/src/main/java/Main.java
+++ mall-common/src/main/java/Main.java
@@ -1,6 +1,3 @@
-import com.baomidou.mybatisplus.core.conditions.Wrapper;
-import com.baomidou.mybatisplus.core.toolkit.Wrappers;
-import com.baomidou.mybatisplus.extension.api.R;
 
 public class Main {
     public static void main(String[] args) {
--- mall-product/src/main/java/com/example/mallproduct/MallProductApplication.java
+++ mall-product/src/main/java/com/example/mallproduct/MallProductApplication.java
@@ -3,7 +3,10 @@ package com.example.mallproduct;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
+@EnableDiscoveryClient
 @MapperScan("com.example.mallproduct.dao")
 @SpringBootApplication
 public class MallProductApplication {
